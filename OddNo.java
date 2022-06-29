cff53b8652d9214468ed573b18ba98f925e04b13
MadhuriK-14 <72243094+MadhuriK-14@users.noreply.github.com>
2022-06-29 15:25:36 +0530

    Create OddNo.java

---
+++ OddNo.java
@@ -0,0 +1,17 @@
+
+public class Hello{
+    public static void main(String[] args) 
+    {
+        int number=45;
+      System. out.println("\n\n The odd numbers upto the number : "+number+" are:");  
+for(int i=1;i<=(number);i++)  
+{  
+  
+    if(number%2!=0)  
+    {  
+         System. out.println(number);  
+    } 
+}  
+  
+    }
+}
